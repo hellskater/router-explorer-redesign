49c8235bd3e19355f894d74a5421500d14b12e83
hellskater <ksrinivasrao531@gmail.com>
2022-08-27 23:05:49 +0530

    transaction details made responsive

--- src/components/transactionDetails/TransactionDetails.jsx
+++ src/components/transactionDetails/TransactionDetails.jsx
@@ -42,11 +42,15 @@ const TransactionDetails = () => {
     return val?.value === destTokenData?.chainId.toString();
   });
 
-  console.log(data);
+  const truncate = (input) => {
+    if (input) {
+      return input.substring(0, 6) + "..." + input?.substring(input.length - 5);
+    } else return input;
+  };
 
   return (
-    <div className="min-h-screen h-full bg-transparent mt-44 p-10 flex flex-col items-center justify-center w-full text-white">
-      <section className="hidden md:flex z-50 justify-between items-center w-[60%]">
+    <div className="min-h-screen h-full bg-transparent mt-44 p-10 flex flex-col items-center justify-center w-full text-white overflow-x-hidden">
+      <section className="hidden lg:flex z-50 justify-between items-center w-[60%]">
         <div className="flex flex-col items-center justify-center">
           <img
             src={srcNetworkData?.icon}
@@ -69,22 +73,23 @@ const TransactionDetails = () => {
       <div className="z-50 flex justify-center items-center mt-14 text-4xl">
         Transaction Details
       </div>
-      <section className="flex flex-col justify-center items-center bg-glass p-8 w-full 2xl:w-[80%] rounded-3xl mt-10 text-xl">
-        <div className="flex items-center w-full m-6">
-          <p className="w-3/12">Date</p>
-          <p className="border-l-[0.2px] border-gray-300 pl-8 text-lg">
+      <section className="flex flex-col justify-center items-center bg-glass p-4 lg:p-8 w-full 2xl:w-[80%] rounded-3xl mt-10 text-lg lg:text-xl">
+        <div className="flex flex-col lg:flex-row justify-center lg:justify-start items-center w-full m-2 lg:m-6">
+          <p className="lg:w-3/12">Date</p>
+          <p className="lg:border-l-[0.2px] border-gray-300 pl-8 text-sm lg:text-lg">
             {data?.created_date}
           </p>
         </div>
-        <div className="flex  items-center w-full m-6">
-          <p className="w-3/12">From</p>
-          <div className="flex items-center border-l-[0.2px] border-gray-300 pl-8 text-lg">
+        <div className="flex flex-col lg:flex-row justify-center lg:justify-start items-center w-full m-2 lg:m-6">
+          <p className="lg:w-3/12">From</p>
+          <div className="flex items-center lg:border-l-[0.2px] border-gray-300 pl-8 text-sm lg:text-lg">
             <img
               src={srcNetworkData?.icon}
               className="h-10 w-10 object-contain mr-3"
               alt="network logo"
             />
-            <p>{data?.depositor_address}</p>
+            <p className="hidden lg:block">{data?.depositor_address}</p>
+            <p className="lg:hidden">{truncate(data?.depositor_address)}</p>
             {clipboard1.copied ? (
               <FaCheck className="text-3xl ml-2 text-green-500" />
             ) : (
@@ -95,15 +100,20 @@ const TransactionDetails = () => {
             )}
           </div>
         </div>
-        <div className="flex items-center w-full m-6">
-          <p className="w-3/12">To</p>
-          <div className="flex items-center border-l-[0.2px] border-gray-300 pl-8 text-lg">
+        <div className="flex flex-col lg:flex-row justify-center lg:justify-start items-center w-full m-2 lg:m-6">
+          <p className="lg:w-3/12">To</p>
+          <div className="flex items-center lg:border-l-[0.2px] border-gray-300 pl-8 text-sm lg:text-lg">
             <img
               src={destNetworkData?.icon}
               className="h-10 w-10 object-contain mr-3"
               alt="network logo"
             />
-            <p>{data?.destination_receipient_address}</p>
+            <p className="hidden lg:block">
+              {data?.destination_receipient_address}
+            </p>
+            <p className="lg:hidden">
+              {truncate(data?.destination_receipient_address)}
+            </p>
             {clipboard2.copied ? (
               <FaCheck className="text-3xl ml-2 text-green-500" />
             ) : (
@@ -116,9 +126,9 @@ const TransactionDetails = () => {
             )}
           </div>
         </div>
-        <div className="flex items-center w-full m-6">
-          <p className="w-3/12">Source Token</p>
-          <div className="flex items-center border-l-[0.2px] border-gray-300 pl-8 text-lg">
+        <div className="flex flex-col lg:flex-row justify-center lg:justify-start items-center w-full m-2 lg:m-6">
+          <p className="lg:w-3/12">Source Token</p>
+          <div className="flex items-center lg:border-l-[0.2px] border-gray-300 pl-8 text-sm lg:text-lg mt-2 lg:mt-0">
             <img
               src={srcTokenData?.logoURI}
               className="h-10 w-10 object-contain mr-3"
@@ -135,15 +145,15 @@ const TransactionDetails = () => {
             )}
           </div>
         </div>
-        <div className="flex items-center w-full m-6">
-          <p className="w-3/12">Source Token Amount</p>
-          <p className="border-l-[0.2px] border-gray-300 pl-8 text-lg">
+        <div className="flex flex-col lg:flex-row justify-center lg:justify-start items-center w-full m-2 lg:m-6">
+          <p className="lg:w-3/12">Source Token Amount</p>
+          <p className="lg:border-l-[0.2px] border-gray-300 pl-8 text-sm lg:text-lg">
             {data?.src_token_amount}
           </p>
         </div>
-        <div className="flex items-center w-full m-6">
-          <p className="w-3/12">Destination Token</p>
-          <div className="flex items-center border-l-[0.2px] border-gray-300 pl-8 text-lg">
+        <div className="flex flex-col lg:flex-row justify-center lg:justify-start items-center w-full m-2 lg:m-6">
+          <p className="lg:w-3/12">Destination Token</p>
+          <div className="flex items-center lg:border-l-[0.2px] border-gray-300 pl-8 text-sm lg:text-lg mt-2 lg:mt-0">
             <img
               src={destTokenData?.logoURI}
               className="h-10 w-10 object-contain mr-3"
@@ -160,15 +170,15 @@ const TransactionDetails = () => {
             )}
           </div>
         </div>
-        <div className="flex items-center w-full m-6">
-          <p className="w-3/12">Destination Token Amount</p>
-          <p className="border-l-[0.2px] border-gray-300 pl-8 text-lg">
+        <div className="flex flex-col lg:flex-row justify-center lg:justify-start items-center w-full m-2 lg:m-6">
+          <p className="lg:w-3/12">Destination Token Amount</p>
+          <p className="lg:border-l-[0.2px] border-gray-300 pl-8 text-sm lg:text-lg ">
             {data?.dest_token_amount}
           </p>
         </div>
-        <div className="flex items-center w-full m-6">
-          <p className="w-3/12">Fee Token</p>
-          <div className="flex items-center border-l-[0.2px] border-gray-300 pl-8 text-lg">
+        <div className="flex flex-col lg:flex-row justify-center lg:justify-start items-center w-full m-2 lg:m-6">
+          <p className="lg:w-3/12">Fee Token</p>
+          <div className="flex items-center lg:border-l-[0.2px] border-gray-300 pl-8 text-sm lg:text-lg">
             <img
               src={feeTokenData?.logoURI}
               className="h-10 w-10 object-contain mr-3"
@@ -177,15 +187,16 @@ const TransactionDetails = () => {
             <p>{data?.feetokensymbol}</p>
           </div>
         </div>
-        <div className="flex items-center w-full m-6">
-          <p className="w-3/12">Source Transaction Hash</p>
-          <div className="flex items-center border-l-[0.2px] border-gray-300 pl-8 text-lg">
+        <div className="flex flex-col lg:flex-row justify-center lg:justify-start items-center w-full m-2 lg:m-6">
+          <p className="lg:w-3/12">Source Transaction Hash</p>
+          <div className="flex items-center lg:border-l-[0.2px] border-gray-300 pl-8 text-sm lg:text-lg">
             <a
               href={srcNetworkData?.scanLink + "/" + data?.deposit_tx_hash}
               className="flex items-center hover:underline underline-offset-4"
               target="_blank"
             >
-              <p>{data?.deposit_tx_hash}</p>
+              <p className="hidden lg:block">{data?.deposit_tx_hash}</p>
+              <p className="lg:hidden">{truncate(data?.deposit_tx_hash)}</p>
               <FiExternalLink className="text-xl ml-1 mb-2" />
             </a>
 
@@ -199,9 +210,9 @@ const TransactionDetails = () => {
             )}
           </div>
         </div>
-        <div className="flex items-center w-full m-6">
-          <p className="w-3/12">Destination Transaction Hash</p>
-          <div className="flex items-center border-l-[0.2px] border-gray-300 pl-8 text-lg">
+        <div className="flex flex-col lg:flex-row justify-center lg:justify-start items-center w-full m-2 lg:m-6">
+          <p className="lg:w-3/12">Destination Transaction Hash</p>
+          <div className="flex items-center lg:border-l-[0.2px] border-gray-300 pl-8 text-sm lg:text-lg">
             <a
               href={
                 destNetworkData?.scanLink + "/" + data?.execute_proposal_tx_hash
@@ -209,7 +220,12 @@ const TransactionDetails = () => {
               target="_blank"
               className="flex items-center hover:underline underline-offset-4"
             >
-              <p>{data?.execute_proposal_tx_hash}</p>
+              <p className="hidden lg:block">
+                {data?.execute_proposal_tx_hash}
+              </p>
+              <p className="lg:hidden">
+                {truncate(data?.execute_proposal_tx_hash)}
+              </p>
               <FiExternalLink className="text-xl ml-1 mb-2" />
             </a>
 
@@ -223,15 +239,15 @@ const TransactionDetails = () => {
             )}
           </div>
         </div>
-        <div className="flex items-center w-full m-6">
-          <p className="w-3/12">Deposit Nonce</p>
-          <p className="border-l-[0.2px] border-gray-300 pl-8 text-lg">
+        <div className="flex flex-col lg:flex-row justify-center lg:justify-start items-center w-full m-2 lg:m-6">
+          <p className="lg:w-3/12">Deposit Nonce</p>
+          <p className="lg:border-l-[0.2px] border-gray-300 pl-8 text-sm lg:text-lg">
             {data?.nonce_id}
           </p>
         </div>
-        <div className="flex items-center w-full m-6">
-          <p className="w-3/12">Status</p>
-          <p className="border-l-[0.2px] border-gray-300 pl-8 text-lg">
+        <div className="flex flex-col lg:flex-row justify-center lg:justify-start items-center w-full m-2 lg:m-6">
+          <p className="lg:w-3/12">Status</p>
+          <p className="lg:border-l-[0.2px] border-gray-300 pl-8 text-sm lg:text-lg">
             {data?.transaction_status}
           </p>
         </div>
